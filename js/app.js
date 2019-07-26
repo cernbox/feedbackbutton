c4d90683014b664b32912f05547df0f42bf0e4de
root <root@cbox-webng-03.cern.ch>
2019-07-26 11:47:25 +0200

    add docs and issue

--- js/app.js
+++ js/app.js
@@ -1,18 +1,52 @@
 
 $(document).ready(function() {
 
-	var url = "https://cern.service-now.com/service-portal/report-ticket.do?name=cernbox-feedback&se=CERNBox-Service";
+	
+	var issue_url = "https://cern.service-now.com/service-portal/report-ticket.do?name=incident&se=CERNBox-Service";
+	var button = $('<li/>')
+		.addClass('issue_button');
 
+	$('<a/>')
+		.addClass('icon-issue')
+		.text("Report an issue")
+		.attr('href', issue_url)
+		.attr('target','_blank')
+		.on('click', function(){
+			window.open(issue_url, '_blank');
+			return false;
+		})
+		.appendTo(button);
+
+	$('#app-navigation ul').append(button);
+
+	var feedback_url = "https://cern.service-now.com/service-portal/report-ticket.do?name=cernbox-feedback&se=CERNBox-Service";
 	var button = $('<li/>')
 		.addClass('feedback_button');
 
 	$('<a/>')
 		.addClass('icon-feedback')
 		.text("Feedback")
-		.attr('href', url)
+		.attr('href', feedback_url)
 		.attr('target','_blank')
 		.on('click', function(){
-			window.open(url, '_blank');
+			window.open(feedback_url, '_blank');
+			return false;
+		})
+		.appendTo(button);
+
+	$('#app-navigation ul').append(button);
+	
+	var doc_url = "http://cernbox-manual.web.cern.ch/cernbox-manual/en/";
+	var button = $('<li/>')
+		.addClass('doc_button');
+
+	$('<a/>')
+		.addClass('icon-doc')
+		.text("Documentation")
+		.attr('href', doc_url)
+		.attr('target','_blank')
+		.on('click', function(){
+			window.open(doc_url, '_blank');
 			return false;
 		})
 		.appendTo(button);
